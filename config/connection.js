02de035139095ba45890b8765ec61075332b403c
Stebbi <stefodd@yahoo.com>
2022-05-01 13:55:39 -0500

    fixed connection issue

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,18 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3001,
+    port: 3306,
     user: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'employees'
+    database: 'employee_db'
 });
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(`Connected to db. ThreadID: ${connection.threadId}`);
+})
 
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -2,25 +2,25 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE department (
+CREATE TABLE department(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles(
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
-    department_id INT 
+    department_id INT, 
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employee(
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    FOREIGN KEY(role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY(role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
-)
+);
