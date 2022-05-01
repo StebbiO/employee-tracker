034d87f71f09d9ddb76b6c4b74844ffc18afa82c
Stebbi <stefodd@yahoo.com>
2022-05-01 14:32:25 -0500

    created initial prompt switch case and outline for functions

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const connection = require('./config/connection');
+
+const openPrompt = () => {
+    inquirer.prompt([
+        {
+            name: 'options',
+            type: 'list',
+            message: 'Please select one of the following options:',
+            loop: false,
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update an Employee Role',
+                'Exit'
+            ]
+        }    
+    ]).then(response => {
+        switch (response.options) {
+            case 'View All Departments':
+                viewDept();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'View All Employees':
+                viewEmp();
+                break;
+            case 'Add a Department':
+                addDept();
+                break;
+            case 'Add a Role':
+                addRole()
+                break;
+            case 'Add an Employee':
+                addEmp();
+                break;
+            case 'Update an Employee Role':
+                updateEmpRole();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    })
+};
+
+openPrompt();
